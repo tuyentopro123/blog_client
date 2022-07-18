6860569921753f99e5358050d88cbd3ed18abcaf
Tuyen <Tuyentopro123@gmail.com>
2022-07-18 21:48:14 +0700

    last commit

--- src/utils/configAxios.js
+++ src/utils/configAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://server-app-js.herokuapp.com/'
+const BASE_URL = 'https://server-app-js.herokuapp.com/'
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
